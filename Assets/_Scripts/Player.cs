edf6d5612b12bd54c8341e076520383c8f55e6bd
A-PLAYER <guomzplayer@gmail.com>
2024-02-27 17:10:51 -0500

    1

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //选项菜单
+    public GameObject OptionsStg1;
+    bool OptionsOpened = false;
+
+    //钱
+    public int point = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //检查选项菜单
+        if(OptionsStg1.activeSelf == true)
+            OptionsStg1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //选项菜单
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OptionMenu();
+        }
+    }
+
+    public void OptionMenu()
+    {
+        if (!OptionsOpened)
+        {
+            OptionsOpened = true;
+            OptionsStg1.SetActive(true);
+        }
+        else
+        {
+            OptionsOpened = false;
+            OptionsStg1.SetActive(false);
+        }
+    }
+}
