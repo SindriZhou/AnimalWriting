5328a2552490b9a8a9813c8ac92f9a6704164551
A-PLAYER <guomzplayer@gmail.com>
2024-02-28 16:21:15 -0500

    1

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -14,21 +15,35 @@ public class Player : MonoBehaviour
     //钱
     public int point = 0;
 
+    //玩家信息
+    public GameObject Stg1_Name, Stg2_Portrait, Stg3_Tag;
+    public GameObject PlayerStat;
+
     //头像
     public static Sprite Portrait;
     public GameObject PorImage;
 
+    //名字
+    public GameObject NameInput, NameText;
+    public static string Name;
+
+    //当前场景
+    public static int SceneNum = 0;
+
     void Start()
     {
+        //关闭玩家信息
+        PlayerStat.SetActive(false);
+
         //检查选项菜单
-        if(OptionsStg1.activeSelf == true)
+        if (OptionsStg1.activeSelf == true)
             OptionsStg1.SetActive(false);
     }
 
     void Update()
     {
-            //选项菜单
-            if (Input.GetKeyDown(KeyCode.Escape))
+        //选项菜单
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             OptionMenu();
         }
@@ -48,9 +63,39 @@ public class Player : MonoBehaviour
         }
     }
 
+    //开始游戏
+    public void GameStart()
+    {
+
+    }
+
+    //玩家信息设置
+    public void SetName()
+    {
+        Debug.Log($"{NameInput.GetComponent<TextMeshProUGUI>().text.Length}");
+
+        if (NameInput.GetComponent<TextMeshProUGUI>().text.Length < 2)
+        {
+        }
+        else
+        {
+            NameText.GetComponent<TextMeshProUGUI>().text = NameInput.GetComponent<TextMeshProUGUI>().text;
+            Stg1_Name.SetActive(false);
+            Stg2_Portrait.SetActive(true);
+        }
+    }
+
     public void SetProtrait(Sprite P)
     {
         Portrait = P;
         PorImage.GetComponent<Image>().sprite = P;
+        Stg2_Portrait.SetActive(false);
+        Stg3_Tag.SetActive(true);
+    }
+
+    public void SetTag()
+    {
+        Stg3_Tag.SetActive(false);
+        PlayerStat.SetActive(true);
     }
 }
---
+++ Assets/_Scripts/Sound/SdM_ui.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SdM_ui : MonoBehaviour
+{
+    public static SdM_ui instance;
+
+    public AudioSource audioSource;
+    [SerializeField]
+    private AudioClip GameStart;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void IsStart()
+    {
+        audioSource.clip = GameStart;
+        audioSource.Play();
+    }
+
+    
+}
