c1f55534a76fcbfa436c55010dc9d9688c06ba04
A-PLAYER <guomzplayer@gmail.com>
2024-04-07 22:27:48 -0400

    1

--- Assets/_Scripts/Click/ClickAndOpen.cs
+++ Assets/_Scripts/Click/ClickAndOpen.cs
@@ -9,13 +9,6 @@ public class ClickAndOpen : MonoBehaviour
 {
     public GameObject A;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
